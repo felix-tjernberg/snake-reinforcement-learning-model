3d98a4e6623ec61557643641098fa7edd8266763
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-05-17 20:32:45 +0200

    Update snake game ui

--- snake_game/snake_game.py
+++ snake_game/snake_game.py
@@ -1,28 +1,29 @@
 from snake_helper_classes import Coordinate, Direction
 import pygame
-import random
+from random import randint
 
 
 class SnakeGame:
     def __init__(self, window_height=480, window_width=480):
         pygame.init()
         self.BLOCK_SIZE = 20
-        self.FONT = pygame.font.Font("./snake_game/arial.ttf", 25)
+        self.FONT = pygame.font.Font("./snake_game/8bit.ttf", 32)
         self.GAME_SPEED = 10
-
-        self.BLACK = (0, 0, 0)
-        self.BLUE = (0, 0, 237)
-        self.RED = (237, 0, 0)
-        self.WHITE = (237, 237, 237)
+        self.COLORS = {"BACKGROUND": (154, 197, 2), "FOREGROUND": (1, 2, 0)}
 
         self.window_height = window_height
         self.window_width = window_width
-        self.display = pygame.display.set_mode((self.window_height, self.window_width))
+        self.display = pygame.display.set_mode(
+            (self.window_height, self.window_width), pygame.NOFRAME
+        )
         pygame.display.set_caption("Maskjävel")
+        self.update_ui = True
+        self.display_border = False
 
         self.clock = pygame.time.Clock()
         self.game_over = False
         self.direction = Direction.RIGHT
+        self.previous_direction = Direction.RIGHT
         self.head = Coordinate(self.window_height / 2, self.window_width / 2)
         self.body = [
             self.head,
@@ -43,8 +44,12 @@ class SnakeGame:
             return self.game_over, self.score
 
         self._check_head_position()
-        self._update_ui()
-        self.clock.tick(self.GAME_SPEED)
+        if self.update_ui:
+            self._update_ui()
+            self.clock.tick(self.GAME_SPEED)
+        else:
+            self.clock.tick()
+
         self._get_user_input()
         return self.game_over, self.score
 
@@ -78,6 +83,16 @@ class SnakeGame:
                     self.direction = Direction.RIGHT
                 elif event.key == pygame.K_UP:
                     self.direction = Direction.UP
+                elif event.key == pygame.K_SPACE:
+                    self.update_ui = not self.update_ui
+                elif event.key == pygame.K_b:
+                    pygame.display.set_mode(
+                        (self.window_height, self.window_width), pygame.SHOWN
+                    )
+                elif event.key == pygame.K_n:
+                    pygame.display.set_mode(
+                        (self.window_height, self.window_width), pygame.NOFRAME
+                    )
 
     def _move(self):
         x = self.head.x
@@ -94,11 +109,11 @@ class SnakeGame:
 
     def _place_food(self):
         x = (
-            random.randint(0, (self.window_height - self.BLOCK_SIZE) // self.BLOCK_SIZE)
+            randint(0, (self.window_height - self.BLOCK_SIZE) // self.BLOCK_SIZE)
             * self.BLOCK_SIZE
         )
         y = (
-            random.randint(0, (self.window_width - self.BLOCK_SIZE) // self.BLOCK_SIZE)
+            randint(0, (self.window_width - self.BLOCK_SIZE) // self.BLOCK_SIZE)
             * self.BLOCK_SIZE
         )
         self.food = Coordinate(x, y)
@@ -106,12 +121,12 @@ class SnakeGame:
             self._place_food()
 
     def _update_ui(self):
-        self.display.fill(self.BLACK)
+        self.display.fill(self.COLORS["BACKGROUND"])
 
         for coordinate in self.body:
             pygame.draw.rect(
                 self.display,
-                self.BLUE,
+                self.COLORS["FOREGROUND"],
                 pygame.Rect(
                     coordinate.x, coordinate.y, self.BLOCK_SIZE, self.BLOCK_SIZE
                 ),
@@ -119,12 +134,12 @@ class SnakeGame:
 
         pygame.draw.rect(
             self.display,
-            self.RED,
+            self.COLORS["FOREGROUND"],
             pygame.Rect(self.food.x, self.food.y, self.BLOCK_SIZE, self.BLOCK_SIZE),
         )
 
-        text = self.FONT.render(f"Score: {self.score}", True, self.WHITE)
-        self.display.blit(text, [0, 0])
+        text = self.FONT.render(f"Score: {self.score}", True, self.COLORS["FOREGROUND"])
+        self.display.blit(text, [16, 16])
         pygame.display.flip()
 
 
