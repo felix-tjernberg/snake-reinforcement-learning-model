b97c47debbccb1f4c7b50dba791371e1175f94dc
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-06-10 13:33:25 +0200

    Start new models and run old models

--- run_model.py
+++ run_model.py
@@ -2,12 +2,14 @@ from snake_gym_environment import SnakeGymEnvironment
 from stable_baselines3 import PPO
 
 environment = SnakeGymEnvironment()
-environment.render()
 
-model_path = f"models/saved_models/PPO_v3_non_linear_step_reward_1654672523_40770000"
+model_name = f"PPO_v3_non_linear_between_food_reward_1_1654770013_143770000"
+environment.snake_game.GAME_SPEED = 30
+
+model_path = f"models/saved_models/{model_name}"
+environment.render()
 loaded_model = PPO.load(model_path, environment)
 
-environment.snake_game.GAME_SPEED = 30
 for episode in range(1, 11):
     observation = environment.reset()
     done = False
--- train_new_model.py
+++ train_new_model.py
@@ -7,9 +7,9 @@ import os
 
 # Change these values before running script
 model_type = PPO
-model_name_prefix = "PPO_test"
-total_timesteps_per_episode = 100000
-model_number = 1
+model_name_prefix = "PPO_v3_keep_direction_reward"
+total_timesteps_per_episode = 1000000
+model_number = 3
 
 
 start_time = int(time.time())
