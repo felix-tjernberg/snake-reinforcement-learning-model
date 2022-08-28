30f30932b31f2e622dd1e22af1a81d02057e1bbb
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-08-28 20:12:34 +0200

    Rename best model

--- run_model.py
+++ run_model.py
@@ -1,4 +1,4 @@
-from snake_gym_environment import SnakeGymEnvironment
+from snake_gym_environment_old import SnakeGymEnvironment
 from stable_baselines3 import PPO
 
 environment = SnakeGymEnvironment()
@@ -6,10 +6,10 @@ environment = SnakeGymEnvironment()
 
 # Change before running model
 environment.snake_game.GAME_SPEED = 30
-MODEL_NAME = f"PPO_v4.2_head_position_1764_float_xy_observations_3_1661703048_5_1000000"
+MODEL_NAME = f"current_best_50_high_score_PPO_v3_keep_direction_reward_4_actions_3_1654867981_305_1000000"
 
 
-model_path = f"models/saved_models/{MODEL_NAME}.zip"
+model_path = f"models/saved_models/{MODEL_NAME}"
 environment.render()
 loaded_model = PPO.load(model_path, environment)
 
@@ -19,6 +19,6 @@ for episode in range(1, 11):
     done = False
     while not done:
         action, _ = loaded_model.predict(observation)
-        _, _, done, _ = environment.step(action)
+        observation, _, done, _ = environment.step(action)
     print(f"Episode {episode} finished with a score of {environment.snake_game.score}")
 environment.close()
